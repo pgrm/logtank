130d1d37f57b385490808229ad1f8e67c3c1db1e
Peter Grman <peter.grman@gmail.com>
2014-11-29 15:32:14 +0100

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('logtankApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
